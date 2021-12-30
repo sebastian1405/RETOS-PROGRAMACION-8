fff9b8fe5c5a68b29232d7ba9b356ecdb1699915
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-12-30 15:06:33 -0300

    Reto iniciado

---
+++ main.js
@@ -0,0 +1,30 @@
+const cosasQueAprendimos = [
+  {
+    tema: 'terminal',
+    class: '',
+  },
+  {
+    tema: 'node',
+    class: '',
+  },
+  {
+    tema: 'oop',
+    class: '',
+  },
+  {
+    tema: 'flexbox',
+    class: '',
+  },
+  {
+    tema: 'css',
+    class: '',
+  },
+  {
+    tema: 'dom',
+    class: 'special',
+  },
+];
+
+function main() {}
+
+main();
