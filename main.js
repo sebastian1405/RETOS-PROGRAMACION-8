4d78b72221ad2d4f8ecee504217a57dab678d714
sebastian guevara <sebastianguevara32569830@gmail.com>
2022-01-18 21:42:37 -0300

    terminado

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 const cosasQueAprendimos = [
   {
-    tema: 'terminal',
-    class: '',
+    tema: "terminal",
+    class: "",
   },
   {
-    tema: 'node',
-    class: '',
+    tema: "node",
+    class: "",
   },
   {
-    tema: 'oop',
-    class: '',
+    tema: "oop",
+    class: "",
   },
   {
-    tema: 'flexbox',
-    class: '',
+    tema: "flexbox",
+    class: "",
   },
   {
-    tema: 'css',
-    class: '',
+    tema: "css",
+    class: "",
   },
   {
-    tema: 'dom',
-    class: 'special',
+    tema: "dom",
+    class: "special",
   },
 ];
 
-function main() {}
+function main() {
+  for (i = 0; i < cosasQueAprendimos.length; i++) {
+    const nuevoitem = document.createElement("li");
+    nuevoitem.className = cosasQueAprendimos[i].class;
+    nuevoitem.innerText = cosasQueAprendimos[i].tema;
+    const ubicado = document.querySelector("ul.lista");
+    ubicado.appendChild(nuevoitem);
+  }
+}
 
 main();
